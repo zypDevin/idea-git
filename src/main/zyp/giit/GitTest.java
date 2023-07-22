8d5b8d5b83765fc52cb6049466dd974066547196
zyp <2754375139@qq.com>
2023-07-22 20:19:08 +0800

    master idea commit

--- src/main/zyp/giit/GitTest.java
+++ src/main/zyp/giit/GitTest.java
@@ -5,5 +5,7 @@ public class GitTest {
         System.out.println("hello~git");
         System.out.println("hello~git2");
         System.out.println("hello~git4");
+        System.out.println("master test");
+
     }
 }
