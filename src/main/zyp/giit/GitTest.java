42743d80a8e5532f319c08fefce63736437776fa
zyp <2754375139@qq.com>
2023-07-22 17:36:20 +0800

    second idea commit

--- src/main/zyp/giit/GitTest.java
+++ src/main/zyp/giit/GitTest.java
@@ -3,5 +3,6 @@ package main.zyp.giit;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello~git");
+        System.out.println("hello~git2");
     }
 }
