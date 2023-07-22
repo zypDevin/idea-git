3c554926411674bfeea5a94b9dace34a5f7ec182
zypDevin <140223604+zypDevin@users.noreply.github.com>
2023-07-22 20:39:00 +0800

    pull  test

--- src/main/zyp/giit/GitTest.java
+++ src/main/zyp/giit/GitTest.java
@@ -10,5 +10,7 @@ public class GitTest {
         System.out.println("hot-fix test");
         System.out.println("push test");
         System.out.println("push ssh test");
+        System.out.println("pull ssh test");
+        
     }
 }
