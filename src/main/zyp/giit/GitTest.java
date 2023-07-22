098b95e3d6f0a2041ae66ab0a2feacf3541fb0ed
zyp <2754375139@qq.com>
2023-07-22 20:31:24 +0800

    push idea commit

--- src/main/zyp/giit/GitTest.java
+++ src/main/zyp/giit/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hello~git5");
         System.out.println("hello~git6");
         System.out.println("hot-fix test");
+        System.out.println("push test");
     }
 }
